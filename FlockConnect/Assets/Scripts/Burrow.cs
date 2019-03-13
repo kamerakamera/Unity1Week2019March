117134de47da829b784d0be1dffcaccf445fbf96
kamera <asahi20000124@icloud.com>
2019-03-13 21:01:54 +0900

    魚の群れが回転も追従するように変更、巣の実装

---
+++ FlockConnect/Assets/Scripts/Burrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Burrow : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private Collider2D col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player"){
+            sprite.material.color = new Color(0,0,0,1);
+            col.enabled = false;
+        }
+    }
+}
--- FlockConnect/Assets/Scripts/Flocks.cs
+++ FlockConnect/Assets/Scripts/Flocks.cs
@@ -7,6 +7,7 @@ public class Flocks : MonoBehaviour
     [SerializeField]
     GameObject followObj;//参照する群れ
     Vector3 nextPos;
+    Quaternion nextRot;
     float waitTime = 0.3f;
     // Start is called before the first frame update
     void Start()
@@ -18,28 +19,29 @@ public class Flocks : MonoBehaviour
     void Update()
     {
         GetFlocksPos();
-        StartCoroutine("CallFlocks",nextPos);
+        StartCoroutine(CallFlocks(nextPos,nextRot));
         
     }
 
     void GetFlocksPos()
     {
         nextPos = followObj.transform.position;
+        nextRot = followObj.transform.rotation;
     }
 
     public void SetFollowObj(GameObject obj){
         followObj = obj;
     }
 
-    void SetFlocksPos(Vector3 pos)
+    void SetFlocksPos(Vector3 pos,Quaternion rot)
     {
         transform.position = pos;
-        
+        transform.rotation = rot;
     }
 
-    private IEnumerator CallFlocks(Vector3 pos){
+    private IEnumerator CallFlocks(Vector3 pos,Quaternion rot){
         yield return new WaitForSeconds(waitTime);
-        SetFlocksPos(pos);
+        SetFlocksPos(pos,rot);
         yield return null;
     }
 }
